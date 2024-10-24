793b4e5224c56e82b1b9d2c27c5897dc7d6d00aa
Juan Pablo Hernandez <jpnator18@gmail.com>
2024-10-24 14:26:06 -0600

    First Commit

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,20 @@
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { deleteItem } from './cartSlice';
+
+function DeleteItem({ pizzaId }) {
+  const dispatch = useDispatch();
+
+  function handleDeleteItem() {
+    console.log(pizzaId);
+    dispatch(deleteItem(pizzaId));
+  }
+
+  return (
+    <Button type="small" onClick={handleDeleteItem}>
+      Delete
+    </Button>
+  );
+}
+
+export default DeleteItem;
---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,28 @@
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { increaseItemQuantity, decreaseItemQuantity } from './cartSlice';
+
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
+
+  function handleIncreaseItemQuantity() {
+    dispatch(increaseItemQuantity(pizzaId));
+  }
+  function handleDecreaseItemQuantity() {
+    dispatch(decreaseItemQuantity(pizzaId));
+  }
+
+  return (
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button type="round" onClick={handleDecreaseItemQuantity}>
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
+      <Button type="round" onClick={handleIncreaseItemQuantity}>
+        +
+      </Button>
+    </div>
+  );
+}
+
+export default UpdateItemQuantity;
