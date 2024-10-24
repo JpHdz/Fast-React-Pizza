c6263c557f8da8ccd626669d74831e2c1d831c87
Juan Pablo Hernandez <jpnator18@gmail.com>
2024-10-24 15:05:01 -0600

    Import Modified

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,4 +1,4 @@
-import Header from './header';
+import Header from './Header';
 import CartOverview from '../features/cart/CartOverview';
 import { Outlet, useNavigation } from 'react-router-dom';
 import Loader from './Loader';
